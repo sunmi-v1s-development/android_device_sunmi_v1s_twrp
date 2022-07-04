193fe580cdb1299209dba79aa5f5c67aa335dc82
soralis0912 <soralis.nem@gmail.com>
2022-07-04 14:37:55 +0900

    V1s-G: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: sunmi Model: V1s-G Script version: 2.3.0

---
+++ omni_V1s-G.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from V1s-G device
+$(call inherit-product, device/sunmi/V1s-G/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := V1s-G
+PRODUCT_NAME := omni_V1s-G
+PRODUCT_BRAND := SUNMI
+PRODUCT_MODEL := V1s-G
+PRODUCT_MANUFACTURER := sunmi
